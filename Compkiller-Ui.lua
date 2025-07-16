e916eff1a4f15991650701faab56a7fa37125053
gwnrdt <jomardelacruz1882@gmail.com>
2025-07-16 20:56:28 +0800

    Update Compkiller-Ui.lua

--- Compkiller-Ui.lua
+++ Compkiller-Ui.lua
@@ -1,9 +1,7 @@
 --[[
-    		Compkiller Interface
+    		Compkiller Interface By Mscuaz
 
-    Author: 4lpaca
-    License: MIT
-    Github: https://github.com/4lpaca-pin/CompKiller
+    Author: Mscuaz
 --]]
 
 --- Export Types ---
